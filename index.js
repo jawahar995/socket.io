fecb51b7885f23cae4c3d4149956a6a758dc4af3
JawaharC_Contus <jawahar.c@contus.in>
2023-06-16 10:16:32 +0530

    base setup

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+const axios = require('axios');
+
+app.use(express.static(path.join(__dirname, '..', 'client-socket')));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('message', async (data) => {
+    try {
+      const location = await fetchAddress(data.location.latitude, data.location.longitude);
+      console.log('Received message:', {...data, ...location});
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+async function fetchAddress(latitude, longitude) {
+  try {
+    const url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`;
+
+    const response = await axios.get(url);
+    const addressData = response.data;
+    return {
+      'osm_id': addressData.osm_id,
+      'city': addressData.address.city,
+      'postcode': addressData.address.postcode,
+      'state': addressData.address.state,
+      'country': addressData.address.country,
+      'country_code': addressData.address.country_code
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const port = 3000;
+http.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
